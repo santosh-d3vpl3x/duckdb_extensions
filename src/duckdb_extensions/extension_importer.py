c4a62d47aa5cb353f6fa2b6b40e8a99a4fce39c6
santosh <santosh@Santoshs-MacBook-Pro.local>
2024-09-10 10:41:18 +0530

    Remove requests from runtime dependency. Also don't keep gz file in the wheel.

--- src/duckdb_extensions/extension_importer.py
+++ src/duckdb_extensions/extension_importer.py
@@ -15,13 +15,10 @@ except ImportError:
 def import_extension(name: str):
     quack_module = importlib.import_module(f"duckdb_extension_{name}")
     module_path = pathlib.Path(str(ilr.files(quack_module)))
-    duckdb_version = duckdb.sql("PRAGMA version;").fetchone()[0]
-    extension_dir = module_path / "extensions" / duckdb_version
-    extension_file_gz = extension_dir / f"{name}.duckdb_extension.gz"
-    extension_file = extension_dir / f"{name}.duckdb_extension"
 
-    block_size = 65536
-    with gzip.open(extension_file_gz, "rb") as s_file, open(extension_file, "wb") as d_file:
-        shutil.copyfileobj(s_file, d_file, block_size)
+    duckdb_version = duckdb.sql("PRAGMA version;").fetchone()[0]
+
+    extension_dir = module_path / "extensions" / duckdb_version
+    extension_file = extension_dir / f"{name}.duckdb_extension"
 
     duckdb.sql(f"INSTALL '{extension_file}'")
